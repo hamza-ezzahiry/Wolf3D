0889179aba10805e8fe204dd80e20d5a200cdad4
Hamza Ezzahiry <hezzahir@e2r9p11.1337.ma>
2019-12-23 23:05:43 +0100

    nothing :(

--- srcs/main.c
+++ srcs/main.c
@@ -1,21 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main2.c                                            :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hezzahir <hamza.ezzahiry@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/19 00:12:01 by hezzahir          #+#    #+#             */
-/*   Updated: 2019/11/16 14:53:28 by hezzahir         ###   ########.fr       */
+/*   Updated: 2019/12/23 23:03:38 by hezzahir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "wolf3d.h"
 
+
+void		draw(t_wolf *w)
+{
+
+}
 int			main(int ac, char **av)
 {
 	t_wolf	w;
-	
+
 	check_ac(ac);
 	if(parse(&w, av[1]) < 0)
 	{
@@ -23,6 +28,7 @@ int			main(int ac, char **av)
 		return 0;
 	}
 	my_mlx_init(&w, av[1]);
+	draw(&w);
 	mlx_hooks(&w);
 	return (0);
 }
